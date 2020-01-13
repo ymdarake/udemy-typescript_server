439d7ff1b75a23bf7c45d7e62fedfed2f59c7e24
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-13 18:47:40 +1300

    [252]Using Property Descriptors for type Checking

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import { AppRouter } from './AppRouter';
