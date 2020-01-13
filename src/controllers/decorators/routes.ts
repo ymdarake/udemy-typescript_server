236107ae8f4c353d3ac5d9537a61000bb377f85d
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-13 18:12:14 +1300

    [244]Closed Method Sets with Enums

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import { Methods } from './Methods';
 
 function routeBinder(method: string) {
   return function get(path: string) {
@@ -9,8 +10,8 @@ function routeBinder(method: string) {
   };
 }
 
-export const get = routeBinder('get');
-export const post = routeBinder('post');
-export const put = routeBinder('put');
-export const patch = routeBinder('patch');
-export const del = routeBinder('del');
+export const get = routeBinder(Methods.get);
+export const post = routeBinder(Methods.post);
+export const put = routeBinder(Methods.put);
+export const patch = routeBinder(Methods.patch);
+export const del = routeBinder(Methods.del);
